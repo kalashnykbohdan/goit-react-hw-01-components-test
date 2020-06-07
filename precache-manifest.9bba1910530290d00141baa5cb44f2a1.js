9d88234c908a42b99b213cc5ff1f4ccc22dbdf46
Kalashnyk Bohdan <space.bohdan@gmail.com>
2020-06-07 15:29:26 +0300

    Updates

---
+++ precache-manifest.9bba1910530290d00141baa5cb44f2a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4111b2399b3beb5522e4e1566ea84cc",
+    "url": "/kalashnykbohdan/goit-js-hw-12-countriesindex.html"
+  },
+  {
+    "revision": "badfd83218187fd70caf",
+    "url": "/kalashnykbohdan/goit-js-hw-12-countriesstatic/css/main.b6dea97f.chunk.css"
+  },
+  {
+    "revision": "44b797f3f338e0cda465",
+    "url": "/kalashnykbohdan/goit-js-hw-12-countriesstatic/js/2.bc091eac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kalashnykbohdan/goit-js-hw-12-countriesstatic/js/2.bc091eac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "badfd83218187fd70caf",
+    "url": "/kalashnykbohdan/goit-js-hw-12-countriesstatic/js/main.5588add3.chunk.js"
+  },
+  {
+    "revision": "71fd67a5538e5ff31ebe",
+    "url": "/kalashnykbohdan/goit-js-hw-12-countriesstatic/js/runtime-main.90816d3d.js"
+  }
+]);
